947c93c5b7e52c8eb99e4acfb55b892419fe969a
mypark <mypark@debian-c1r2s3.mypark.42.kr>
2022-12-06 05:30:02 -0600

    statistics

---
+++ calc_statistics.c
@@ -0,0 +1,27 @@
+#include "ft_ping.h"
+
+extern struct global g_;
+
+double ft_babylonian(double a, int cnt) {
+  if (cnt == 0) return 1.0;
+  double n_1 = ft_babylonian(a, cnt - 1);
+  return (n_1 * n_1 + a) / (2 * n_1);
+}
+
+void calc_statistics(double time) {
+  g_.max = g_.max > time ? g_.max : time;
+  g_.min = g_.min < time ? g_.min : time;
+
+  double total_time = g_.avg * (g_.total_count - 1) + time;
+  double curr_avg = total_time / g_.total_count;
+  double prev_squre_sum =
+      (g_.mdev * g_.mdev + g_.avg * g_.avg) * (g_.total_count - 1);
+  double variance =
+      (prev_squre_sum + time * time) / g_.total_count - curr_avg * curr_avg;
+
+  g_.avg = curr_avg;
+  g_.mdev = ft_babylonian(variance, 5);
+
+  double beta = 0.5;
+  g_.ewma = g_.ewma * beta + (1 - beta) * time;
+}
